1ad1ecfa06f12d235b1cf12f4c20c084ead5bd24
burakc3tin <burakcetin9724@gmail.com>
2023-07-26 14:41:44 +0300

    v1

--- App.js
+++ App.js
@@ -1,20 +1,108 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect,useState } from 'react';
+import { View, Text,FlatList,StyleSheet,SafeAreaView,StatusBar,Linking,ActivityIndicator } from 'react-native';
+ import {  Card, Button, Icon } from '@rneui/themed';
+ 
+ const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
 
-export default function App() {
+  const [news,setNews] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = () => {
+    fetch("https://api.collectapi.com/news/getNews?country=tr&tag=general", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": ""
+      }
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        console.log(data.result[0])
+        setNews(data.result)
+        setIsLoading(false);
+
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
+  
+  if (isLoading) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center',backgroundColor:'rgba(78, 116, 289, 1)' }}>
+       
+       <Text style={{color:'white',fontSize:20,marginBottom:10}}>En Ekspress Haber</Text>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <>
+       <StatusBar/>
+       <Button  buttonStyle={{
+                backgroundColor: 'rgba(78, 116, 289, 1)',
+               }}>
+  En Ekspress Haber
+  <Icon style={{marginLeft:4}} name="circle" color="white" />
+</Button>
+        <FlatList 
+      style={styles.container}
+      data={news}
+      renderItem={({item})=> <Card>
+      <Card.Title>{item.name}</Card.Title>
+      <Card.Divider />
+      <Card.Image
+        style={{ padding: 0 }}
+        source={{
+          uri:
+            item.image,
+        }}
+      />
+      <Text style={{ marginBottom: 10 }}>
+       {item.description}
+      </Text>
+      <Text style={{ marginBottom: 10,fontStyle:'italic',fontWeight:100 }}> {item.date.substring(0,10)}</Text>
 
+      <Text style={{ marginBottom: 10,fontStyle:'italic',fontWeight:100 }}>Kaynak: {item.source}</Text>
+      <Button
+      onPress={()=>Linking.openURL(item.url)}
+        icon={
+          <Icon
+            name="link"
+            color="#ffffff"
+            iconStyle={{ marginRight: 10 }}
+          />
+        }
+        buttonStyle={{
+          borderRadius: 0,
+          marginLeft: 0,
+          marginRight: 0,
+          marginBottom: 0,
+        }}
+        title="Detaylar..."
+      />
+    </Card>}
+      keyExtractor={(item)=>item.key}
+      />
+
+      
+ 
+     </>
+  );
+};
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  container:{
+    backgroundColor:'wheat'
   },
-});
+  row:{
+    padding:15,
+    marginBottom:5,
+    backgroundColor:'skyblue'
+  }
+})
+export default App;
